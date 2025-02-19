373e34bbecc24d424b0b968d7502c88d3a116c5c
dvlboo <kuhhhckr@gmail.com>
2025-02-19 20:18:08 +0700

    biner data

--- utils/loss_function.py
+++ utils/loss_function.py
@@ -5,11 +5,7 @@
 
 
 import tensorflow as tf
-from tensorflow.keras.applications import VGG19
 from tensorflow.keras.losses import BinaryCrossentropy
-
-from keras.models import Model
-
 
 # In[2]:
 
@@ -20,25 +16,12 @@ alpha = 100
 def l1_loss(y_true, y_pred):
 	return tf.reduce_mean(tf.abs(y_true - y_pred))
 
-# Perceptual Loss
-def perceptual_loss(y_true, y_pred):
-	vgg = VGG19(include_top=False, weights='imagenet', input_shape=(256, 256, 1))
-	vgg.trainable = False
-	
-	for layer in vgg.layers:
-		layer.trainable = False
-	model = Model(inputs=vgg.input, outputs=vgg.get_layer('block5_conv4').output)
-	model.trainable = False
-
-	return tf.reduce_mean(tf.square(model(y_true) - model(y_pred)))
-
 # Generator Loss
 def generator_loss(y_true, y_pred, fake_output):
 	adv_loss = BinaryCrossentropy(from_logits=True)(tf.ones_like(fake_output), fake_output)
 	l1 = l1_loss(y_true, y_pred)
-	perc = perceptual_loss(y_true, y_pred)
 
-	total_loss = alpha * l1 + perc + adv_loss
+	total_loss = alpha * l1 + adv_loss
 	return total_loss
 
 # Discriminator Loss
